67448dfa9ca80f533f8c01c64b9ffdf119f3865f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-01-27 00:32:21 +0100

    Use Module#dynamic_method for ASM dsl.

--- lib/convinius/asm.rb
+++ lib/convinius/asm.rb
@@ -3,31 +3,11 @@ module Convinius
     extend self
 
     def asm(file = "(asm)", line = 1, method = :call, &block)
-      g = Rubinius::Generator.new
-      g.name = method.to_sym
-      g.file = file.to_sym
-      g.set_line line
-
-      g.required_args = 0
-      g.total_args = 0
-      g.splat_index = nil
-
-      g.local_count = 0
-      g.local_names = []
-
-      block.arity > 0 ? yield(g) : g.instance_eval(&block)
-
-      g.ret
-      g.close
-
-      g.encode
-      cm = g.package Rubinius::CompiledMethod
-      puts cm.decode if $DEBUG
-
       code = Object.new
-      ss = Rubinius::StaticScope.new Object
-      Rubinius.attach_method g.name, cm, ss, code
-
+      Rubinius.object_metaclass(code).dynamic_method(method, file, line) do |g|
+        block.arity > 0 ? yield(g) : g.instance_eval(&block)
+        g.ret
+      end
       code
     end
 
