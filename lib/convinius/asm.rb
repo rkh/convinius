b46306468830cad2d29472c75d4855b5eb2bb6f2
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-01-27 00:39:05 +0100

    asm now takes a options hash

--- lib/convinius/asm.rb
+++ lib/convinius/asm.rb
@@ -2,13 +2,14 @@ module Convinius
   module ASM
     extend self
 
-    def asm(file = "(asm)", line = 1, method = :call, &block)
-      code = Object.new
-      Rubinius.object_metaclass(code).dynamic_method(method, file, line) do |g|
+    def asm(opts = {}, &block)
+      object = opts[:object] || Object.new
+      opts = { :file => "(asm)", :line => 1, :method => :call }.merge! opts
+      Rubinius.object_metaclass(object).dynamic_method(opts[:method], opts[:file], opts[:line]) do |g|
         block.arity > 0 ? yield(g) : g.instance_eval(&block)
         g.ret
       end
-      code
+      object
     end
 
     class << self
