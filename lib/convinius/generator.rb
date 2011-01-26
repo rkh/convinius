84641c5d54a88dba576258ebd1116d2c2c78894e
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-01-27 00:22:58 +0100

    better implementation for make_tuple (avoids dispatch and creating an array)

--- lib/convinius/generator.rb
+++ lib/convinius/generator.rb
@@ -3,10 +3,16 @@ module Convinius
     append_features Rubinius::Generator
 
     def make_tuple(count)
-      push_rubinius
-      find_const :Tuple
-      move_down count
-      send :[], count
+      push count
+      invoke_primitive :tuple_allocate, 1
+      (count - 1).downto(0) do |i|
+        dup
+        rotate 3
+        push i
+        swap
+        invoke_primitive :tuple_put, 3
+        pop
+      end
     end
   end
 end
